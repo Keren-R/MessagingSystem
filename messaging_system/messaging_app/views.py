b192779ccd6d2273bb23f97134186a49991f5c9a
keren <kerenres7@gmail.com>
2024-04-11 14:26:05 +0300

    implemented read message

--- messaging_system/messaging_app/views.py
+++ messaging_system/messaging_app/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -16,11 +14,10 @@ class MessagesList(APIView):
     """
     def get(self, request, format=None):
         """
-        Return a list of all messages.
+        Return a list of all sent or received messages by the logged-in user.
         """
         user = self.request.user
         messages = Message.objects.filter(Q(sender_id=user) | Q(receiver_id=user))
-
         serializer = MessageSerializer(messages, many=True)
 
         return Response(serializer.data)
@@ -34,17 +31,41 @@ class MessagesList(APIView):
             user_name = request.data.get('receiver')
             receiver = User.objects.get(username=user_name)
             serializer.save(sender=request.user, receiver=receiver)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            data = serializer.data
+            response_status = status.HTTP_201_CREATED
+        else:
+            data = serializer.errors
+            response_status = status.HTTP_400_BAD_REQUEST
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data, status=response_status)
 
 
 class MessageDetailView(APIView):
+    """
+    View for displaying and manipulate a single message.
+    """
     def get(self, request, pk):
+        """
+        Return the message object with the provided pk.
+        """
         try:
             message = Message.objects.get(pk=pk)
             serializer = MessageSerializer(message)
-            return Response(serializer.data)
+            data = serializer.data
+            response_status = status.HTTP_200_OK
+        except Message.DoesNotExist:
+            data = None
+            response_status = status.HTTP_404_NOT_FOUND
+
+        return Response(data=data, status=response_status)
+
+    def put(self, request, pk):
+        try:
+            message = Message.objects.get(pk=pk)
+            message.is_read = True
+            message.save()
+            serializer = MessageSerializer(message)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Message.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -71,15 +92,3 @@ class UnreadMessagesList(APIView):
         serializer = MessageSerializer(messages, many=True)
 
         return Response(serializer.data)
-
-# class UsersList(APIView):
-#     """
-#     View to list all messages.
-#     """
-#     def get(self, request, format=None):
-#         """
-#         Return a list of all users.
-#         """
-#         users = User.objects.all()
-#         serializer = UserSerializer(users, many=True)
-#         return Response(serializer.data)
