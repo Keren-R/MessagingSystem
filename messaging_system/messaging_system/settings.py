f1a9db7273069e60acd0368cdfb825bc5f6a38a3
keren <kerenres7@gmail.com>
2024-04-13 19:25:40 +0300

    deploy to pythonanywhere setup

--- messaging_system/messaging_system/settings.py
+++ messaging_system/messaging_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p^i51b^hgi&7o)ekut-@$_oa7t+)xdx+iqnxju=(qp^ahz6kp+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'keren.pythonanywhere.com']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.storage.CompressedStaticFilesStorage'
 ]
 
 ROOT_URLCONF = 'messaging_system.urls'
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
